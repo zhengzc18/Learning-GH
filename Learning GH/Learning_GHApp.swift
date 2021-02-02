792fdf72cb0b91fcfe39bce3fdc646962cf379de
Jaesoon <zhengzch18oon@gmail.com>
2021-02-02 16:58:00 +0800

    Initial Commit

---
+++ Learning GH/Learning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GHApp.swift
+//  Learning GH
+//
+//  Created by JAESOON on 2021/02/02.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
